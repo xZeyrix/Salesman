937b0f1994c6323aa03a1f568508a166cd34ebd7
Zeyrix <nikolas.chelyuk@gmail.com>
2026-04-22 12:57:34 +0000

    ADDED: base files - main, config, requirements, env & a prototype of AI logic.

---
+++ ai_project/main.py
@@ -0,0 +1,22 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import Command
+import logging
+import sys
+from config import settings
+import asyncio
+from router import router
+
+dp = Dispatcher()
+
+async def main() -> None:
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    bot = Bot(token=settings.bot_token, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+    dp.include_router(router)
+
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
